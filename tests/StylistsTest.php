df83b9231335639ecad8f8e5c036eb5229821842
Charles Peden <ccbpeden@warpmail.net>
2017-02-24 09:06:11 -0800

    add test for setters

--- tests/StylistsTest.php
+++ tests/StylistsTest.php
@@ -26,5 +26,26 @@
 
           $this->assertEquals([$specialty, $stylist_last_name, $stylist_first_name], $result);
         }
+
+        function test_setters()
+        {
+            $stylist_last_name = "Chiznople";
+            $stylist_first_name = "Frederique";
+            $specialty = "facial";
+            $new_stylist = new Stylist($stylist_last_name, $stylist_first_name, $specialty);
+            $replacement_last_name = "Dorquelin";
+            $replacement_first_name = "Panopticus";
+            $replacement_specialty = "weaves";
+
+            $new_stylist->setLastName($replacement_last_name);
+            $new_stylist->setFirstName($replacement_first_name);
+            $new_stylist->setSpecialty($replacement_specialty);
+
+            $result = array();
+            array_push($result, $new_stylist->getLastName(), $new_stylist->getFirstName(), $new_stylist->getSpecialty());
+
+            $this->assertEquals([$replacement_last_name, $replacement_first_name, $replacement_specialty], $result);
+
+        }
     }
 ?>
