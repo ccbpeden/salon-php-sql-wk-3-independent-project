43f523b4b84a6671e61e4143115b876ea0fcc108
Charles Peden <ccbpeden@warpmail.net>
2017-02-24 09:16:42 -0800

    add test for sanitize function

--- tests/StylistsTest.php
+++ tests/StylistsTest.php
@@ -45,6 +45,20 @@
             array_push($result, $new_stylist->getLastName(), $new_stylist->getFirstName(), $new_stylist->getSpecialty());
 
             $this->assertEquals([$replacement_last_name, $replacement_first_name, $replacement_specialty], $result);
+        }
+
+        function test_sanitize()
+        {
+            $stylist_last_name = "D'Souza";
+            $stylist_first_name = "L&Broni'que";
+            $specialty = "straightening, perms, dying";
+            $new_stylist = new Stylist($stylist_last_name, $stylist_first_name, $specialty);
+
+            $new_stylist->sanitize();
+            $result = array()
+            array_push($result, $new_stylist->getLastName(), $new_stylist->getFirstName());
+
+            $this->assertEquals([], $result);
 
         }
     }
